fe09eaad31f8b9e63c912c8e1e2428cf9b3b4cb6
Nick Collins <necollins03@gmail.com>
2026-04-17 15:52:58 -0400

    Initial Commit

---
+++ FinalProject.v1/FinalProject_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProject_v1App.swift
+//  FinalProject.v1
+//
+//  Created by Nick Collins on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalProject_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
