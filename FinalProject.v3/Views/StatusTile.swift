422cc2112163c4bacf1eeef62323f53c65e13ea8
Nick Collins <necollins03@gmail.com>
2026-04-23 23:13:08 -0400

    StatusTile View Update

---
+++ FinalProject.v3/Views/StatusTile.swift
@@ -0,0 +1,18 @@
+//
+//  StatusTile.swift
+//  FinalProject.v2
+//
+//  Created by Nick Collins on 4/23/26.
+//
+
+import SwiftUI
+
+struct StatusTile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatusTile()
+}
